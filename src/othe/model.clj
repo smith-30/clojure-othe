6a59ece4f6b92ac5cc7df87b25736913d7d01d91
smith-30 <smith-30@example.com>
2024-03-07 11:44:46 +0900

    mod: 桁見出し

--- src/othe/model.clj
+++ src/othe/model.clj
@@ -1,1 +1,20 @@
 (ns othe.model)
+
+(def b-size 8)
+(def first-pos 0)
+(def last-pos (* b-size b-size))
+(def all-pos (range first-pos last-pos))
+
+(def first-col 0)
+(def last-col b-size)
+(def first-row 0)
+(def last-row b-size)
+
+(defn col-from-pos [pos] (mod pos b-size))
+(defn row-from-pos [pos] (quot pos b-size))
+(defn pos-from-rowcol [r c] (+ (* r b-size) c))
+
+(def dirs #{:n :ne :e :se :s :sw :w :nw})
+
+(def board (ref [])) ; 盤面状態ベクタ
+(def player (ref nil)) ; 次の手番。:b か :w
--- src/othe/view.clj
+++ src/othe/view.clj
@@ -1,3 +1,12 @@
 (ns othe.view
   (:use othe.model
         [clojure.string :only (join)]))
+
+(def code-a 97) ; a の文字コード
+(def code-curly 123) ; z の次の文字コード
+(def col-headers
+  (take b-size
+        (map (comp str char)
+             (range code-a code-curly))))
+(def col-header-str
+  (str " " (join " " col-headers)))
